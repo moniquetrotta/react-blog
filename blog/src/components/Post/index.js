aab7367021699197bb497b5af6e7b757d34a032c
moniquetrotta <monique.trotta@gmail.com>
2022-04-08 18:25:49 -0300

    atualização de componente post

--- blog/src/components/Post/index.js
+++ blog/src/components/Post/index.js
@@ -1,9 +1,18 @@
-const Post = () =>{
-  return(
+const Post = () => {
+  return (
     <>
       <div className="img-post"></div>
-      <h3>Help!</h3>
+      <h5>Help!</h5>
       <h4>A solution to soften people's fear</h4>
+      <p>
+        This case study is part of my third project done for the Google course on UX Design Certificate, in which it was a free case study, but focused on Social Goods</p>
+        <div>
+          <div className="img-profile"></div>
+          <div className="description-profile">
+            <h6>Lucas Ossola</h6>
+            <p>Aug 2, 2022 - 8 min read</p>
+          </div>
+        </div>
     </>
   );
 }
