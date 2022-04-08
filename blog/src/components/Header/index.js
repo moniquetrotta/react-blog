330e437a0c69091906a4d683f8ed9a718700abec
moniquetrotta <monique.trotta@gmail.com>
2022-04-08 15:17:21 -0300

    update scss do blog

---
+++ blog/src/components/Header/index.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return(
+    <>
+    <header>
+      <div className="logo"></div>
+      <div className="search"></div>
+      <div className="menu"></div>
+    </header>
+    </>
+  );
+}
+
+export default Header;
